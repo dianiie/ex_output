deb3bfbbf03557f87d2e1ca47892eaff622ff5fa
dianiie <81855246+dianiie@users.noreply.github.com>
2021-04-04 10:54:52 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,12 +4,29 @@ function CW () {
         control.waitMicros(20000)
     }
 }
+function Window_Close () {
+    dgr = 90
+    for (let index = 0; index < 80; index++) {
+        pins.servoWritePin(AnalogPin.P16, dgr)
+        dgr += -1
+        control.waitMicros(20000)
+    }
+}
+function Window_Open () {
+    dgr = 10
+    for (let index = 0; index < 80; index++) {
+        pins.servoWritePin(AnalogPin.P16, dgr)
+        dgr += 1
+        control.waitMicros(20000)
+    }
+}
 function CCW () {
     for (let index = 0; index <= 180; index++) {
         pins.servoWritePin(AnalogPin.P16, 180 - index)
         control.waitMicros(20000)
     }
 }
-pins.servoWritePin(AnalogPin.P16, 0)
-CW()
-CCW()
+let dgr = 0
+Window_Close()
+basic.pause(1000)
+Window_Open()
